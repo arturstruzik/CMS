720d580343ea5c56dacaa7cdd62373a0af5c51ab
Artur Struzik <info@arturstruzik.pl>
2016-09-26 14:18:15 +0200

    Initial commit.

---
+++ FutureNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FutureNet.Startup))]
+namespace FutureNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
