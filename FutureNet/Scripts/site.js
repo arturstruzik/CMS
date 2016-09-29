3daf9b284a1e6b9ed7bd821527af7da597f3da64
Artur Struzik <info@arturstruzik.pl>
2016-09-29 13:43:48 +0200

    Set empty layout navigation settings.

---
+++ FutureNet/Scripts/site.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+    $(document).scroll(function () {
+
+        if ($(window).scrollTop() > 50) {
+            $(".fixed-top").css('top', '0px');
+            $(".fixed-top").css('position', 'fixed');
+        }
+        if ($(window).scrollTop() <= 50) {
+            $(".fixed-top").css('top', '50px');
+            $(".fixed-top").css('position', 'absolute');
+        }
+    });
+});
