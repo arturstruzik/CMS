571610569434e9daa9e0cbe321be9f2c3398b7bf
Artur Struzik <info@arturstruzik.pl>
2016-09-29 22:46:51 +0200

    Added site top bar.

--- FutureNet/Scripts/site.js
+++ FutureNet/Scripts/site.js
@@ -2,12 +2,12 @@
 
     $(document).scroll(function () {
 
-        if ($(window).scrollTop() > 50) {
+        if ($(window).scrollTop() > 90) {
             $(".fixed-top").css('top', '0px');
             $(".fixed-top").css('position', 'fixed');
         }
-        if ($(window).scrollTop() <= 50) {
-            $(".fixed-top").css('top', '50px');
+        if ($(window).scrollTop() <= 90) {
+            $(".fixed-top").css('top', '90px');
             $(".fixed-top").css('position', 'absolute');
         }
     });
